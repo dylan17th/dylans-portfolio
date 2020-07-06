d7da6fe70ba317a256620cb8ab075f3ac566f819
Dylan Collins <dcollins5590@students.mc3.edu>
2020-07-06 09:45:48 -0400

    added a build command

---
+++ dylan_collins_portfolio/build/precache-manifest.129fba02a5e8e71042dcb26a61f300dc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a97d0ff9c40919396438f0a7f7bdb67e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7370870e31a892b757b9",
+    "url": "/static/css/main.e2f9d2e3.chunk.css"
+  },
+  {
+    "revision": "9176efc88d3d43cea42a",
+    "url": "/static/js/2.342db9fb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.342db9fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7370870e31a892b757b9",
+    "url": "/static/js/main.d4587300.chunk.js"
+  },
+  {
+    "revision": "c22acc328becd78ecd0b",
+    "url": "/static/js/runtime-main.be0ec195.js"
+  },
+  {
+    "revision": "3ec11cf7ed4a045a3bb6c8d7f74640e0",
+    "url": "/static/media/Dylan.3ec11cf7.png"
+  },
+  {
+    "revision": "e74cca9bae95d709949698b1f25a6c0c",
+    "url": "/static/media/Dylan_M_Collins_Resume.e74cca9b.pdf"
+  },
+  {
+    "revision": "3a62e503b19716815c6253f7ae178f55",
+    "url": "/static/media/activities.3a62e503.png"
+  },
+  {
+    "revision": "b766668d63846128bf47b377a3df0c41",
+    "url": "/static/media/newdashboard.b766668d.png"
+  },
+  {
+    "revision": "fa26143b78e16fee720c6dd877a92e03",
+    "url": "/static/media/newsettings.fa26143b.png"
+  },
+  {
+    "revision": "e4a316feea2a3daf19517fd336c1990d",
+    "url": "/static/media/signup.e4a316fe.png"
+  }
+]);
